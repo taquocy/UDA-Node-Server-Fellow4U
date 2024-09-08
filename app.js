70e06cc2f5074c8d8f60155a325d9590766ed934
y Pi4 <taquocitali@gmail.com>
2024-09-08 08:25:43 +0700

    add permission to share image to client

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const { app, sequelize, config } = require('./config/config');  // Đảm bảo đường dẫn là chính xác
 const tourRoutes = require('./routes/tourRoutes');
+const express = require('express');
+const path  = require('path');  
 
-
-app.use('/resource/images', express.static(path.join(__dirname, 'images')));
+app.use('/resource/images', express.static(path.join(__dirname,"resource", 'images')));
 // Sử dụng các route
 app.use('/api', tourRoutes);
 
